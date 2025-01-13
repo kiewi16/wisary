00594ba04a8b80327cd7af7fcba09b99a447018b
Kim Ewing <pekar.kimberly@gmail.com>
2025-01-13 11:45:22 -0700

    add dummpyProjects data and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
-import { Button } from "@radix-ui/themes";
+// import { Button } from "@radix-ui/themes";
+import { useState } from "react";
 import ProjectCard from './ProjectCard.tsx'
 
 function App() {
+  const dummyProjects = [
+    { id: 1, title: "Project Alpha"},
+    { id: 2, title: "Project Beta"},
+    { id: 3, title: "Project Charlie"},
+    { id: 4, title: "Project Delta"},
+    { id: 5, title: "Project Echo"}
+  ]
+  const [projects, setProjects] = useState([])
+
+
   return (
     <div className="App">
       <header className="App-header">
